1f18fec1a754ab606e5debd4a2fc444fa43ffc1e
HUe1998 <wasddsawwasddsawwasd@gmail.com>
2021-05-15 22:06:32 +0530

    Use Mongo to provide views & add create post view

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const path = require('path');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
 const app = express();
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
-
+mongoose.connect('mongodb://localhost:27017/nodejs_blog',
+    {useNewUrlParser: true});
 app.listen(4000, () => {
   console.log('App listening on port 4000');
 });
@@ -27,4 +28,14 @@ app.get('/index', (req, res) => {
 
 app.get('/post', (req, res) => {
   res.render('post');
+});
+
+// Get Handler for new post
+app.get('/posts/new',(req,res)=>{
+  res.render('create');
+});
+
+app.post('/posts/store',(req,res)=>{
+  console.log(req.body);
+  res.redirect('/');
 });
