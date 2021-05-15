b1282040feb6d9ba56cfeeff013c7735ff4f4b12
HUe1998 <wasddsawwasddsawwasd@gmail.com>
2021-05-15 18:07:09 +0530

    Refactor code to dynamic render HTML using EJS
    Use EJS Templating Engine to refactor our code such that repeating
    components between pages like 'navbar', 'footer' need not be rendered
    again & again.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Blogging Site with Node.js as backend
+<hr/>
+Aim of this project is to use Node.js, Express and Mongodb to deploy a site on heroku.
--- index.js
+++ index.js
@@ -1,28 +1,30 @@
 const express = require('express');
 const path = require('path');
+const ejs = require('ejs');
 const app = express();
 app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
 app.listen(4000, () => {
   console.log('App listening on port 4000');
 });
 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+  res.render('index');
 });
 
 app.get('/about', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'pages/about.html'));
+  res.render('about');
 });
 
 app.get('/contact', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'pages/contact.html'));
+  res.render('contact');
 });
 
 app.get('/index', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+  res.render('index');
 });
 
 app.get('/post', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'pages/post.html'));
+  res.render('post');
 });
