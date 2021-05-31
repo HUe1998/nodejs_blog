cf100897e0884f238645f8f1381a34871e8e9deb
HUe1998 <himanshu.1998me@gmail.com>
2021-05-31 20:17:47 +0530

    link post from 'new post' to MongoDB

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const express = require('express');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
 const app = express();
+const bodyParser = require('./node_modules/body-parser');
+const BlogPost = require('./models/BlogPost.js');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 mongoose.connect('mongodb://localhost:27017/nodejs_blog',
-    {useNewUrlParser: true});
+  { useNewUrlParser: true });
 app.listen(4000, () => {
   console.log('App listening on port 4000');
 });
@@ -31,11 +36,11 @@ app.get('/post', (req, res) => {
 });
 
 // Get Handler for new post
-app.get('/posts/new',(req,res)=>{
+app.get('/posts/new', (req, res) => {
   res.render('create');
 });
 
-app.post('/posts/store',(req,res)=>{
-  console.log(req.body);
+app.post('/posts/store', async (req, res) => {
+  await BlogPost.create(req.body);
   res.redirect('/');
 });
